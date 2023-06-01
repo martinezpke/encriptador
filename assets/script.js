dd33021813974bee0344b4818a51e7c3d74045e1
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-01 14:19:23 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
+var muneco = document.querySelector(".img__muneco");
+var text_1 = document.getElementById("text_1");
+var text_2 = document.getElementById("text_2");
+var text_result = document.querySelector(".text__result");
+var container_result = document.querySelectorAll(".ocultar");
+
 function encriptarBtn() {
-	var input = document.getElementById("input").value;
+	ocurtar()
+	var input = document.getElementById("container__text").value;
 	var output = encriptar(input);
-	document.getElementById("input_2").value = output;
+	text_result.textContent = output;
+	document.getElementById("container__text").value = "";
 }
 
 function desencriptarBtn() {
-	var input = document.getElementById("input_2").value;
+	ocurtar()
+	var input = document.getElementById("container__text").value;
 	var output = desencriptar(input);
-	document.getElementById("input_2").value = output;
+	text_result.textContent = output;
 }
 
 function encriptar(text) {
@@ -30,3 +39,22 @@ function desencriptar(text) {
 	}
 	return text
 }
+
+function ocurtar(){
+	muneco.classList.add("ocultar");
+	text_1.classList.add("ocultar");
+	text_2.classList.add("ocultar");
+	container_result[0].classList.add("activate");
+	container_result[1].classList.add("activate");
+	text_result.classList.add("container__result")
+
+}
+
+const btnCopy = document.getElementById('btn__copy');
+	btnCopy.addEventListener('click', copiar = () => {
+	var content = document.querySelector('.text__result').textContent;
+	navigator.clipboard.writeText(content)
+	alert('Texto copiado: ' + content);
+  });
+
+  
